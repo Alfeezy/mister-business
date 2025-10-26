d74be0b35967f89f6ab8a7341143c2b2de505ded
Bastien Gliech <bastiengliech@gmail.com>
2025-10-26 14:44:54 -0400

    Upgrade apscheduler version

--- app/main.py
+++ app/main.py
@@ -5,27 +5,21 @@ from fastapi.staticfiles import StaticFiles
 
 import uuid
 
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.datastores.sqlalchemy import SQLAlchemyDataStore
+from apscheduler.triggers.calendarinterval import CalendarIntervalTrigger
+from apscheduler import Scheduler, task
 
 from pydantic import BaseModel
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
-from zoneinfo import ZoneInfo
 
-from .services.receipt_printer_service import print_chore
+from .services.receipt_printer_service import print_chore, ensure_connection
 
 # scheduler
-jobstores = {
-  'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
-}
-scheduler = BackgroundScheduler(
-  jobstores=jobstores,
-  timezone='America/New_York'
-)
-scheduler.start()
+datastore = SQLAlchemyDataStore('sqlite:///jobs.sqlite')
+scheduler = Scheduler(datastore)
+scheduler.start_in_background()
 
 # model for creating chores
 class Chore(BaseModel):
@@ -38,6 +32,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+@task(misfire_grace_time=100000)
 def print_job(description):
   print_chore(description)
   sleep(5)
@@ -60,12 +55,12 @@ async def get_index(request: Request):
 @app.get("/chores/", response_class=HTMLResponse)
 async def get_chores(request: Request):
   chorelist = []
-  for job in scheduler.get_jobs():
+  for schedule in scheduler.get_schedules():
     chorelist.append({
-      "id": job.id,
-      "description": job.name,
-      "interval": job.trigger.interval.days,
-      "next_run": job.next_run_time
+      "id": schedule.id,
+      "description": schedule.args[0],
+      "interval": schedule.trigger.days,
+      "next_run": schedule.next_fire_time
     })
   chorelist.sort(key=lambda item: item['next_run'])
   return templates.TemplateResponse("chores/index.html", {"request": request, "chorelist": chorelist})
@@ -77,47 +72,55 @@ async def new_chore(request: Request):
 
 @app.post("/chores/")
 async def create_chore(request: Request, description: str = Form(...), start: datetime = Form(...), interval: int = Form(...)):
-  job_id = uuid.uuid1()
-  scheduler.add_job(
-    print_job,
-    'interval',
-    id=str(job_id),
-    name=description,
-    args=[description],
+  schedule_id = uuid.uuid1()
+  trigger = CalendarIntervalTrigger(
     start_date=start,
-    days=interval
+    days=interval,
+    timezone="America/New_York"
   )
+
+  scheduler.add_schedule(
+    print_job,
+    trigger,
+    id=str(schedule_id),
+    args=[description],
+  )
+
   return RedirectResponse(url="/chores", status_code=303)
 
 @app.get("/chore/{id}/edit", response_class=HTMLResponse)
 async def edit_chore(request: Request, id: str):
-  job = scheduler.get_job(id)
+  schedule = scheduler.get_schedule(id)
   chore = Chore(
-    description=job.name,
-    start=job.trigger.start_date,
-    interval=job.trigger.interval.days
+    description=schedule.args[0],
+    start=schedule.trigger.start_date,
+    interval=schedule.trigger.days
   )
   return templates.TemplateResponse("chores/edit.html", {"request": request, "chore": chore, "id": id})
 
 @app.post("/chore/{id}")
 async def update_chore(request: Request, id: str, description: str = Form(...), start: datetime = Form(...), interval: int = Form(...)):
-  job = scheduler.get_job(id)
-  job.modify(
-    name=description
-  )
-  job.reschedule(
-    'interval',
+  scheduler.remove_schedule(id)
+  trigger = CalendarIntervalTrigger(
     start_date=start,
-    days=interval
+    days=interval,
+    timezone="America/New_York"
+  )
+
+  scheduler.add_schedule(
+    print_job,
+    trigger,
+    id=id,
+    args=[description],
   )
   return RedirectResponse(url="/chores", status_code=303)
 
 @app.post("/chore/{id}/run_chore")
 async def run_chore(request: Request, id: str):
-  job = scheduler.get_job(id)
+  schedule = scheduler.get_schedule(id)
   current_dt = datetime.now()
-  run_dt = job.next_run_time
-  new_dt = (current_dt + job.trigger.interval).replace(
+  run_dt = schedule.next_fire_time
+  new_dt = (current_dt + timedelta(days=schedule.trigger.days)).replace(
     hour=run_dt.hour,
     minute=run_dt.minute,
     second=0,
@@ -125,13 +128,22 @@ async def run_chore(request: Request, id: str):
   )
 
   # trigger now
-  print_chore(job.name)
+  print(schedule.args[0])
 
-  # reset next chore time to later date
-  job.reschedule(
-    'interval',
-    start_date=new_dt
+  # reset next chore time to later date 
+  trigger = CalendarIntervalTrigger(
+    start_date=new_dt,
+    days=schedule.trigger.days,
+    timezone="America/New_York"
   )
+  scheduler.remove_schedule(id)
+  scheduler.add_schedule(
+    print_job,
+    trigger,
+    id=id,
+    args=schedule.args,
+  )
+
   return RedirectResponse(url="/chores", status_code=303)
 
 
@@ -147,8 +159,8 @@ async def print(request: Request, description: str = Form(...)):
 @app.delete("/chore/{id}")
 @app.post("/chore/{id}/delete")
 async def delete_chore(id: str):
-  chore = scheduler.get_job(id)
+  chore = scheduler.get_schedule(id)
   if chore == None:
     raise HTTPException(status_code=404, detail="Chore not found")
-  scheduler.remove_job(id)
+  scheduler.remove_schedule(id)
   return RedirectResponse(url="/chores", status_code=303)
